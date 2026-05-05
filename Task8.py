57a94963e8f0264b2901610789541dbb606e63f6
KevinMei381 <24210@burnside.school.nz>
2026-05-06 10:03:47 +1200

    1 or 2?

--- Task8.py
+++ Task8.py
@@ -10,8 +10,29 @@ def print_all():
         cursor.execute(sql)
         results = cursor.fetchall()
         # print nicely
+        print('')
         for ID in results:
-            print(f'{ID[0]:<2} {ID[1]:<10} {ID[2]:<20} {ID[3]:<8} {ID[4]}')
+            print(f'{ID[0]:<2} {ID[1]:<10} {ID[2]:<30} {ID[3]:<8} {ID[4]}')
 
 
-print_all()
+def print_all_desc():
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+        sql = "SELECT * FROM Veges ORDER BY AverageDays DESC;"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        # print nicely
+        print('')
+        for ID in results:
+            print(f'{ID[0]:<2} {ID[1]:<10} {ID[2]:<30} {ID[3]:<8} {ID[4]}')
+
+
+apple = input('1 or 2?')
+try:
+    int(apple)
+    if apple == '1':
+        print_all_desc()
+    elif apple == '2':
+        print_all()
+except ValueError:
+    print('no')
